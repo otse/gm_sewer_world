64f83382d2477b39808ea99a33bc343bb201ea7a
Okay <1123362+otse@users.noreply.github.com>
2023-03-11 21:00:33 +0100

    New icon

--- tools/drop to update icon.bat
+++ tools/drop to update icon.bat
@@ -1,2 +1,2 @@
-"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -icon "%~dpn1.jpg" -id "2830508570"
+"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -icon "%~dpn1.jpg" -id "2945489802"
 pause
